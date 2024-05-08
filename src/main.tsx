427b644534fea0f1cec86e3b229bf5e078c53504
Nishant Sinha <nishantsinha.er@gmail.com>
2024-05-09 00:16:00 +0530

    third commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,20 @@ import App from './App.tsx'
 import './index.css'
 import { FronteggProvider } from '@frontegg/react';
 
+//https://{your_domain}.com/?**organization**={account_name}
+// http://localhost:5173/?organization=NS18
+const tenantResolver= () => {
+  const urlQueryParams = new URLSearchParams(window.location.search);
+  const organization = urlQueryParams.get('organization');
+  return {
+    tenant: organization
+  }
+};
+  
 const contextOptions = {
   baseUrl: 'https://app-2jfu1rjwgj7b.frontegg.com',
-  clientId: 'd9ae496e-9d20-4fdc-880b-db2c11b8654d'
+  clientId: 'd9ae496e-9d20-4fdc-880b-db2c11b8654d',
+  tenantResolver,
 };
 
 const authOptions = {
